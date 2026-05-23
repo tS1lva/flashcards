9b8fa7ce5cf4fbe2665e861788a8831551704cd1
Tiago <tiago.ps2@puccampinas.edu.br>
2026-05-23 19:34:19 -0300

    metodo review funcionando, pendente refatorar

--- src/main/java/com/tiago/flashcards/service/FlashService.java
+++ src/main/java/com/tiago/flashcards/service/FlashService.java
@@ -1,9 +1,12 @@
 package com.tiago.flashcards.service;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.tiago.flashcards.entity.FlashcardEntity;
 import com.tiago.flashcards.repository.FlashRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -44,6 +47,45 @@ public class FlashService {
 
     public void deleteAll () {
         flashRepository.deleteAll();
+    }
+
+    public List<FlashcardEntity> review () {
+        List<FlashcardEntity> allFlashcards = this.list();
+        List<FlashcardEntity> toBeReviewd = this.list();
+        toBeReviewd.clear();
+
+        LocalDate actual = LocalDate.now();
+        int actualDay = actual.getDayOfMonth();
+        int actualMonth = actual.getMonthValue();
+        int actualYear = actual.getYear();
+
+        LocalDate a = LocalDate.of(actualYear, actualMonth, actualDay);
+
+
+        for (FlashcardEntity flashcard : allFlashcards) {
+
+            int flashcardDay = flashcard.getNextTime().getDayOfMonth();
+            int flashcardMonth = flashcard.getNextTime().getMonthValue();
+            int flashcardYear = flashcard.getNextTime().getYear();
+
+            LocalDate b = LocalDate.of(flashcardYear, flashcardMonth, flashcardDay);
+
+
+            if (a.isAfter(b)) {
+
+                toBeReviewd.add(flashcard);
+                System.out.println("nao cheguei aqui");
+            }
+        }
+
+        if (toBeReviewd.isEmpty()) {
+            System.out.println("Lista Vazia");
+            return allFlashcards;
+        } else {
+            System.out.println("Lista Cheia");
+            return toBeReviewd;
+        }
+
     }
 
 
